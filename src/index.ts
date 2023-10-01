e0449861cb762a83e62b04216fd0747ae83156ba
Mike Handberg Hovedskov <deengo113@gmail.com>
2023-10-01 15:11:26 +0200

    test verification

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,36 @@
 import { Elysia } from "elysia";
+import { jwt } from "@elysiajs/jwt";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => `${process.env.SECRET}`).listen(3000);
+const app = new Elysia()
+  .use(
+    jwt({
+      name: "jwt",
+      secret: process.env.SECRET!,
+    })
+  )
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "Webdock Auth",
+          description: "API Documentation",
+          version: "0.0.1",
+        },
+      },
+    })
+  )
+  .get("/", () => `${process.env.SECRET}`)
+  .get("/verify", async ({ jwt, body }) => {
+    console.log(body);
+
+    const { t } = body as { t: string };
+
+    const verification = await jwt.verify(t);
+
+    return verification;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
