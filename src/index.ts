aae167253b31f6497f2def92c1d537bec7cac7b8
Mike Handberg Hovedskov <deengo113@gmail.com>
2023-10-01 14:49:59 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import { Elysia } from "elysia";
 const app = new Elysia().get("/", () => `${process.env.SECRET}`).listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-  `${process.env.SECRET}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
